01419b912146d140be02e6e44b34c483ed552beb
Roberto  Albelda <robertoalbeldac@gmail.com>
2022-09-05 14:10:56 +0800

    init

---
+++ nuxt.config.js
@@ -0,0 +1,35 @@
+export default {
+  head: {
+    title: 'careconnect-front',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  css: [],
+
+  plugins: [],
+
+  components: false,
+
+  buildModules: [
+    '@nuxtjs/eslint-module',
+  ],
+
+  modules: [
+    '@nuxtjs/axios',
+  ],
+
+  axios: {
+    baseURL: '/',
+  },
+
+  build: {},
+}
