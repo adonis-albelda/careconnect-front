7d3fdf951fbb58300ef40146684b3be08c2e321d
Roberto  Albelda <robertoalbeldac@gmail.com>
2022-10-02 22:14:11 +0800

    added firebase config

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,57 +12,92 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap' }
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap',
+      },
     ],
   },
-  target: "server",
+  target: 'server',
   server: {
     port: 9797,
-    host: "0.0.0.0", // default: 3000,
+    host: '0.0.0.0', // default: 3000,
   },
   auth: {
     strategies: {
-      local: {  
+      local: {
         token: {
-            property: 'access_token',
-            global: true,
+          property: 'access_token',
+          global: true,
         },
         user: {
-            property: '',
+          property: '',
         },
         tokenType: '',
         endpoints: {
-            login: { url: 'login', method: 'post' },
-            logout: { url: 'logout', method: 'get' },
-            user: { url: 'user/details', method: 'get' }
-        }
-    }
+          login: { url: 'login', method: 'post' },
+          logout: { url: 'logout', method: 'get' },
+          user: { url: 'user/details', method: 'get' },
+        },
+      },
     },
-    
   },
-  css: [
-    "~assets/scss/global.scss",
-  ],
+  css: ['~assets/scss/global.scss'],
   styleResources: {
-    scss: ["./assets/scss/*.scss"],
+    scss: ['./assets/scss/*.scss'],
   },
   plugins: [
-    "~global/components/VueSelect.js",
-    "~global/components/VueDatepicker.js",
-    {src:"~global/components/VueDialog.js", ssr:false, mode: 'client'},
-    {src:"~global/components/VueNotification.js", ssr:false, mode: 'client'},
-    "~global/utils/index.js",
-    {src:"~global/plugins/VeeValidate.js", ssr:false}
-    
+    '~global/components/VueSelect.js',
+    '~global/components/VueDatepicker.js',
+    { src: '~global/components/VueDialog.js', ssr: false, mode: 'client' },
+    {
+      src: '~global/components/VueNotification.js',
+      ssr: false,
+      mode: 'client',
+    },
+    '~global/utils/index.js',
+    { src: '~global/plugins/VeeValidate.js', ssr: false },
   ],
   components: false,
   // buildModules: ['@nuxtjs/eslint-module'],
-  modules: ['@nuxtjs/auth-next', '@nuxtjs/axios'],
+  modules: ['@nuxtjs/auth-next', '@nuxtjs/axios', '@nuxtjs/firebase'],
   axios: {
-    baseURL: process.env.BASE_URL
+    baseURL: process.env.BASE_URL,
   },
   router: {
     middleware: ['auth'],
+  },
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyAVLCKeCrFQzGWxstZIIY3aVprd1E82Y6Y',
+      authDomain: 'care-connect-e6828.firebaseapp.com',
+      projectId: 'care-connect-e6828',
+      storageBucket: 'care-connect-e6828.appspot.com',
+      messagingSenderId: '252064637090',
+      appId: '1:252064637090:web:c9d2fad08d03b6ec2b77fb',
+      measurementId: 'G-YLKD04C876',
+    },
+    services: {
+      messaging: {
+        createServiceWorker: true,
+        actions: [
+          {
+            action: 'randomName',
+            url: 'randomUrl',
+          },
+        ],
+        fcmPublicVapidKey:
+          'BFzKvuGgjsX-u1QSlhMSmco6-21hf2E0zfn3fP6uTqu2tTgLi_OO92OaH2iWgDkgJ01uNQOy-2XbrydCb_ZlO1s',
+      },
+      auth: {
+        // initialize: {
+        //   onAuthStateChangedAction: 'onAuthStateChanged',
+        // },
+        ssr: true,
+        emulatorPort: undefined,
+        disableEmulatorWarnings: false,
+      },
+    },
   },
   build: {},
 }
